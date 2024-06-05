deef663929928a19788c55148d5023882c81c79d
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-06-05 19:39:38 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,22 +11,22 @@ export default async function Home() {
               alt="Al Siam Profile"
               className="shadow-dark"
             />
-            <h1>Your Name</h1>
-            <p>Your expertise</p>
+            <h1>Rasel Shikdar</h1>
+            <p>Web Developer</p>
             <div className="social-links">
-              <a href="https://twitter.com/" target="_blank">
+              <a href="https://twitter.com/raselshikdar_" target="_blank">
                 <i className="fa fa-twitter" />
               </a>
-              <a href="https://facebook.com/" target="_blank">
+              <a href="https://facebook.com/raselshiikdar" target="_blank">
                 <i className="fa fa-facebook" />
               </a>
-              <a href="https://github.com/" target="_blank">
+              <a href="https://github.com/raselshikdar" target="_blank">
                 <i className="fa fa-github" />
               </a>
-              <a href="https://instagram.com/" target="_blank">
+              <a href="https://instagram.com/raselshiikdar" target="_blank">
                 <i className="fa fa-instagram" />
               </a>
-              <a href="https://linkedin.com/in/" target="_blank">
+              <a href="https://linkedin.com/in/rasel597" target="_blank">
                 <i className="fa fa-linkedin" />
               </a>
             </div>
