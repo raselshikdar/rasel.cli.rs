f8bed95743362f52c11f7f139a9fcdfc2659bbc8
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-06-06 10:25:01 +0600

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,6 +3,32 @@ import type { NextPage } from "next";
 const Contact: NextPage = () => {
   return (
     <>
+      export default function Contact() {
+      async function handleSubmit(event) {
+          event.preventDefault();
+          const formData = new FormData(event.target);
+
+          formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+          const object = Object.fromEntries(formData);
+          const json = JSON.stringify(object);
+
+          const response = await fetch("https://api.web3forms.com/submit", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+              },
+              body: json
+          });
+          const result = await response.json();
+          if (result.success) {
+              console.log(result);
+          }
+      }
+
+    return (
+      <>
       <section className="contact section" id="contact">
         <div className="container">
           <div className="row">
