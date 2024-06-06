1b43cef87d48396efe632733147e2fa0b30fb97c
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-06-06 13:11:39 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ import ThemeSwitch from "@/components/panel/ThemeSwitch";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "urFolio - Portfolio web",
-  description: "urFolio - a Open Source portfolio web application",
+  title: "Rasel Shikdar - Portfolio web",
+  description: "rasel.cli.rs - a Open Source portfolio web application about Rasel Shikdar",
 };
 
 export default function RootLayout({
